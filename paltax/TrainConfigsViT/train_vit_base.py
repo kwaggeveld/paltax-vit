536e70ab612bf56f59029a695f3bb4240e564a95
Koen Waggeveld <k.c.waggeveld@student.rug.nl>
2026-04-20 10:20:20 +0200

    Added vit_base config in preparation for grid search. Can now specify model hyperparameters using --config.param=value.

---
+++ paltax/TrainConfigsViT/train_vit_base.py
@@ -0,0 +1,35 @@
+import pathlib
+from paltax.TrainConfigs import train_config_npe_base
+from ml_collections.config_dict import FieldReference, placeholder
+
+'''
+    Base configuration for training ViT models in paltax. 
+    Uses base NPE configuration with COSMOS input config,
+    higher steps_per_epoch and lower learning rate.
+    
+
+    Use as:
+        python main.py \
+            --config=/path/to/train_vit_base.py \
+            --config.model=ModelName \
+            --config.other_fields=value \
+            --workdir=/path/to/workdir
+
+   
+'''
+
+
+def get_config():
+    """Get the hyperparameter configuration"""
+    config = train_config_npe_base.get_config()
+
+    # Overwrite input configuration to created cosmos catalogue
+    config.input_config_path = str(pathlib.Path(__file__).parent)
+    config.input_config_path += '/input/input_cosmos_train.py'
+
+    config.steps_per_epoch = FieldReference(15600)
+    config.learning_rate = 1e-4
+
+    config.model = placeholder(str)     # model name should be set through the `--config.model=model_name` flag
+
+    return config
